d61e2263d69b0ac36a08e51a5a4fffe276b7a47d
Максим <sterh84@gmail.com>
2024-07-07 12:27:38 +0300

    Сделана сетка и PP для мобильной версии

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,15 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+
+// // Функция для загрузки iframe с видео
+// function loadVideo() {
+//   const videoContainer = document.querySelector('.about__image');
+//   videoContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/watch?v=9TZXsZItgdw" frameborder="0" allowfullscreen></iframe>';
+// }
+
+// // Добавляем обработчик события на кнопку "Play Video"
+// document.querySelector('.about__video-button').addEventListener('click', () => {
+//   loadVideo(); // Вызываем функцию загрузки видео
+// });
